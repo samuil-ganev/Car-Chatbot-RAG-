7358b7efde29ecce22195c10c2c031ede07a791a
Samuil Ganev <samuil_ganev@epam.com>
2025-05-07 14:56:45 +0000

    Update 3 files
    - /frontend/ask_llm.py
    - /frontend/llm_connector.py
    - /frontend/search.py

--- frontend/ask_llm.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:66e73af1b9a44b25049ea8091f5954b2e6c132672c93f86ee95d730f8c5d2585
-size 2898
--- frontend/search.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:30b365f4632fcdf2a4074fc5c0e037f3d91096a8061477fe0faa370ce20f2dbc
-size 3447
