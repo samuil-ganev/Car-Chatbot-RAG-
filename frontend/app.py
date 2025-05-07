d146a9c881183556bcd24486887ce1221a0d80de
Samuil Ganev <samuil_ganev@epam.com>
2025-05-07 15:23:52 +0300

    New feature: chat history

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2232048fb00cc7f18572d12a6aa0642a554a384e2db9789f32724bf222f7811
-size 5116
+oid sha256:102daa50a6ff43e44918e3d8fa7b66075cca5c73c8fc27f3115dbe5404e42afd
+size 6744
--- frontend/search.py
+++ frontend/search.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93c8271489168e551b4489ac1f9015cc82b0d59e19ad4fc20cbcb803fa7c4119
-size 4908
+oid sha256:30b365f4632fcdf2a4074fc5c0e037f3d91096a8061477fe0faa370ce20f2dbc
+size 3447
